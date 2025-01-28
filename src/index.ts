3130ce7119f633d1653c63d14053d55aeee28678
KalmonJ <kalmonkk69@gmail.com>
2025-01-28 00:48:10 -0300

    chore: tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 export { chunkUpload } from "./s3/upload/chunk-upload";
 export {
   presignedUrlUpload,
-  S3Config,
-  PutObjectCommandConfig,
-  RequestPresigningArguments,
-  S3ClientConfig,
+  type S3Config,
+  type PutObjectCommandConfig,
+  type RequestPresigningArguments,
+  type S3ClientConfig,
 } from "./s3/upload/url-presigned-upload";
 export {
   AWSProvider,
-  AWSProviderProps,
-  UploadBaseProps,
-  UrlPresignedUploadProps,
+  type AWSProviderProps,
+  type UploadBaseProps,
+  type UrlPresignedUploadProps,
 } from "./s3/upload/provider";
 export { getValidMimeType } from "./utils/mime-type";
